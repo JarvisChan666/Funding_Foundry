0d7a1dde0c646378eff5bf5943ff41d1104e4344
JarvisChan666 <744765386@qq.com>
2024-04-27 18:20:54 +0800

    feat: add reentrant: FundMe.sol

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -10,7 +10,7 @@ pragma solidity ^0.8.18;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 
-error FundMe_NotOwner();
+error FundMe_NotOwner(); // Error handling, more gas efficnent than 'require'
 
 contract FundMe {
     using PriceConverter for uint256;
@@ -18,66 +18,87 @@ contract FundMe {
     mapping(address => uint256) public addressToAmountFunded;
     address[] public funders;
 
-    // Could we make this constant?  /* hint: no! We should make it immutable! */
-    address public /* immutable */ i_owner;// Immutable owner who deploy this
+    // Marked as immutable if the owner will not change, to save on gas costs.
+    address public immutable i_owner; // Immutable owner who deploy this
     uint256 public constant MINIMUM_USD = 5e18;
     AggregatorV3Interface private s_priceFeed;
 
     // The constructor initializes the contract with an address of the Chainlink Price Feed,
     // which varies based on the network the contract is deployed on.
     constructor(address priceFeed) {
-        // If we just deploy FundMe, 
+        // If we just deploy FundMe,
         // i_owner will be our own address
         i_owner = msg.sender;
         s_priceFeed = AggregatorV3Interface(priceFeed);
     }
 
     function fund() public payable {
-        require(msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD, "You need to spend more ETH!");
+        require(
+            msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD,
+            "You need to spend more ETH!"
+        );
         // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
         addressToAmountFunded[msg.sender] += msg.value;
         funders.push(msg.sender);
     }
-    
-    function getVersion() public view returns (uint256){
+
+    function getVersion() public view returns (uint256) {
         return s_priceFeed.version();
     }
-    
-    modifier onlyOwner {
-        // require(msg.sender == owner);
+
+    modifier onlyOwner() {
         if (msg.sender != i_owner) revert FundMe_NotOwner();
         _;
     }
-    
-    function withdraw() public onlyOwner {
-        for (uint256 funderIndex=0; funderIndex < funders.length; funderIndex++){
+
+    bool private isWithdrawing;
+
+    modifier nonReentrant() {
+        require(!isWithdrawing, "No reentrancy");
+        isWithdrawing = true;
+        _;
+        isWithdrawing = false;
+    }
+
+    function withdraw() public onlyOwner nonReentrant(){
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
         }
         funders = new address[](0);
         // // transfer
         // payable(msg.sender).transfer(address(this).balance);
-        
+
         // // send
         // bool sendSuccess = payable(msg.sender).send(address(this).balance);
         // require(sendSuccess, "Send failed");
 
         // call
-        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
         require(callSuccess, "Call failed");
     }
     // Explainer from: https://solidity-by-example.org/fallback/
     // Ether is sent to contract
     //      is msg.data empty?
-    //          /   \ 
+    //          /   \
     //         yes  no
     //         /     \
-    //    receive()?  fallback() 
-    //     /   \ 
+    //    receive()?  fallback()
+    //     /   \
     //   yes   no
     //  /        \
     //receive()  fallback()
 
+    /*
+     ensures that any accidental ETH sent to the contract 
+     using send or transfer still calls the fund() function.
+    */
     fallback() external payable {
         fund();
     }
@@ -85,7 +106,6 @@ contract FundMe {
     receive() external payable {
         fund();
     }
-
 }
 
 // Concepts we didn't cover yet (will cover in later sections)
