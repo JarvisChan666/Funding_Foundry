3f9b88342d7b55607222429a9fd38aaacd742a5c
JarvisChan666 <744765386@qq.com>
2024-04-27 14:27:58 +0800

    feat: add polygonEth pricefeed

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -26,7 +26,9 @@ contract HelperConfig {
         }
     }
 
-    // pure: not read and modified
+    // pure: the function won't read and modified contract's state
+    // Only return hardcode data ! ! !
+    // If return configuration data that depends on a stored value or a state variable, they should be marked as 'view'
     function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
         NetworkConfig memory sepoliaConfig = NetworkConfig({
             priceFeed: 0xCf45E7346f604C883cE36AEAc6CaeC5f45f2fe09
@@ -36,10 +38,16 @@ contract HelperConfig {
 
     function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
         NetworkConfig memory mainnetEthConfig = NetworkConfig({
-            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+            priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
         });
         return mainnetEthConfig;
     }
 
+    function getPolygonEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory polygonEthConfig = NetworkConfig({
+            priceFeed: 0xF9680D99D6C9589e2a93a78A04A279e509205945
+        });
+        return polygonEthConfig;
+    }
     function getAnvilEthConfig() public pure returns (NetworkConfig memory) {}
 }
