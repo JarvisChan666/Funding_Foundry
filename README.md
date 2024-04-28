608320af68e183954d149e92bca96a6a32c05e4d
JarvisChan666 <744765386@qq.com>
2024-04-28 20:29:48 +0800

    feat(fundme): added emit inreceive and fallback

--- README.md
+++ README.md
@@ -3,3 +3,12 @@ forge install smartcontractkit/chainlink-brownie-contracts@0.6.1 --no-commit
 forge test -m xxx -vvv --fork-url
 
 forge test --fork-url $MAINNET_RPC_URL
+
+## How to git comment
+feat(tests): add comprehensive FundMe tests
+
+- Implement testFundAndBalanceUpdate to verify contract balance increases correctly after funding.
+- Develop testFunderIsAddedToList to ensure funders array is updated accurately post-funding.
+- Establish testWithdrawalByOwner to check only owner can withdraw and their balance updates.
+
+These tests aim to improve the reliability and coverage of FundMe contract functionality.
--- src/FundMe.sol
+++ src/FundMe.sol
@@ -10,118 +10,93 @@ pragma solidity ^0.8.18;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 
-error FundMe_NotOwner(); // Error handling, more gas efficnent than 'require'
+/// Custom error for easier identification and gas saving.
+error FundMe_NotOwner();
 
+/// @title A funding contract with minimum USD threshold
+/// @dev This contract allows users to fund ETH and withdraw it by owner. It utilizes a Chainlink Price Feed for conversion rates.
+/// @author jarvischan
 contract FundMe {
     using PriceConverter for uint256;
 
+    // State variables
     mapping(address => uint256) public addressToAmountFunded;
     address[] public funders;
-
-    // Marked as immutable if the owner will not change, to save on gas costs.
-    address public immutable i_owner; // Immutable owner who deploy this
+    address public immutable owner;
     uint256 public constant MINIMUM_USD = 5e18;
-    AggregatorV3Interface private s_priceFeed;
+    bool private locked;
+    AggregatorV3Interface private priceFeed;
+    event FundReceived(address sender, uint256 amount);
 
-    // The constructor initializes the contract with an address of the Chainlink Price Feed,
-    // which varies based on the network the contract is deployed on.
-    constructor(address priceFeed) {
-        // If we just deploy FundMe,
-        // i_owner will be our own address
-        i_owner = msg.sender;
-        s_priceFeed = AggregatorV3Interface(priceFeed);
+    modifier onlyOwner() {
+        if (msg.sender != owner) revert FundMe_NotOwner();
+        _;
     }
 
+    modifier nonReentrant() {
+        require(!locked, "No reentrancy");
+        locked = true;
+        _;
+        locked = false;
+    }
+
+    constructor(address priceFeedAddress) {
+        // If we just deploy FundMe,
+        // owner will be our own address
+        owner = msg.sender;
+        priceFeed = AggregatorV3Interface(priceFeedAddress);
+    }
+
+    /// @notice Allows users to fund the contract with ETH based on the set USD value.
+    /// @dev The amount of ETH is converted to USD using the current price feed value.
     function fund() public payable {
         require(
-            msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD,
-            "You need to spend more ETH!"
+            msg.value.getConversionRate(priceFeed) >= MINIMUM_USD,
+            "Insufficient ETH!"
         );
         // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
         addressToAmountFunded[msg.sender] += msg.value;
         funders.push(msg.sender);
     }
 
-    function getVersion() public view returns (uint256) {
-        return s_priceFeed.version();
-    }
-
-    function getFunderCount() public view returns (uint256) {
-        return funders.length;
-    }
-
-    modifier onlyOwner() {
-        if (msg.sender != i_owner) revert FundMe_NotOwner();
-        _;
-    }
-
-    bool private isWithdrawing;
-    // existing state variables...
-    mapping(address => uint256) private addressToFunderIndex;
-
-    // Prevent attacker's contract is able to repeatedly call the withdraw function before it finishes execution
-    // potentially draining the contract's funds.
-    modifier nonReentrant() {
-        require(!isWithdrawing, "No reentrancy");
-        isWithdrawing = true;
-        _;
-        isWithdrawing = false;
-    }
-
+    /// @notice Withdraws all funds from the contract sending them to the owner.
+    /// @dev This function uses nonReentrancy modifier to prevent reentrant attacks.
     function withdraw() public onlyOwner nonReentrant {
-        for (
-            uint256 funderIndex = 0;
-            funderIndex < funders.length;
-            funderIndex++
-        ) {
-            address funder = funders[funderIndex];
+        address[] memory fundersCopy = funders;
+        for (uint256 i = 0; i < fundersCopy.length; i++) {
+            address funder = fundersCopy[i];
             addressToAmountFunded[funder] = 0;
         }
         funders = new address[](0);
-        // // transfer
-        // payable(msg.sender).transfer(address(this).balance);
-
-        // // send
-        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
-        // require(sendSuccess, "Send failed");
-
-        // "call" to send all eth to sender address
-        (bool callSuccess, ) = payable(msg.sender).call{
-            value: address(this).balance
-        }("");
-        require(callSuccess, "Call failed");
+        (bool success, ) = payable(owner).call{value: address(this).balance}(
+            ""
+        );
+        require(success, "Transfer failed");
     }
-    // Explainer from: https://solidity-by-example.org/fallback/
-    // Ether is sent to contract
-    //      is msg.data empty?
-    //          /   \
-    //         yes  no
-    //         /     \
-    //    receive()?  fallback()
-    //     /   \
-    //   yes   no
-    //  /        \
-    //receive()  fallback()
 
+    /// @notice Returns the current price feed version.
+    function getPriceFeedVersion() public view returns (uint256) {
+        return priceFeed.version();
+    }
+
+    // Fallback and receive functions to handle direct ETH transfers
     /*
-    when contract reveive eth but don't call any function
-     ensures that any accidental ETH sent to the contract 
-     using send or transfer still calls the fund() function.
+    The difference is: 
+    the Receive function is only called when the contract receives a transaction, 
+    while the Fallback function, in addition to being called when the contract receives a transaction, is also called when there is no matching function in the contract or when additional data needs to be sent to the contract.
     */
     fallback() external payable {
         fund();
+        emit FundReceived(msg.sender, msg.value);
     }
 
     receive() external payable {
         fund();
+        emit FundReceived(msg.sender, msg.value);
+    }
+
+    // Optional getter for the size of the funders array.
+    function getFunderCount() public view returns (uint256) {
+        return funders.length;
     }
 }
-
-// Concepts we didn't cover yet (will cover in later sections)
-// 1. Enum
-// 2. Events
-// 3. Try / Catch
-// 4. Function Selector
-// 5. abi.encode / decode
-// 6. Hash with keccak256
-// 7. Yul / Assembly
