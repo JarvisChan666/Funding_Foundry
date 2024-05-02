0a4febacd273f80615e03fb0d0396b0164d85bf7
JarvisChan666 <744765386@qq.com>
2024-05-02 16:10:12 +0800

    fix(*): fixed interaction test, deleted fundme interaction contract to fund fundme contract by using FUNDER directly, make it more readable

--- test/integration/InteractionsTest.t..sol
+++ test/integration/InteractionsTest.t..sol
@@ -6,7 +6,7 @@ pragma solidity ^0.8.19;
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../../src/FundMe.sol";
 import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
-import {FundContract, WithdrawContract} from "../../script/Interactions.s.sol";
+import {WithdrawContract} from "../../script/Interactions.s.sol";
 
 /// @title FundMe Integrations Tests
 /// @notice This contract implements tests for the FundMe contract using the Forge framework for Solidity testing.
@@ -31,9 +31,13 @@ contract InteractionsTest is Test {
         */
     }
 
-    function testUserCanWithdrawFunds() public {
+    
+    function testUserCanFundInteractions() public {
         // Arrange
+
+        // Ensure FUNDER has enough funds to send
         vm.deal(FUNDER, fundAmount);
+
         uint256 preFundBalance = address(fundMe).balance;
         uint256 preFundFunderBalance = address(FUNDER).balance;
         uint256 preFundOwnerBalance = fundMe.i_owner().balance;
@@ -43,12 +47,16 @@ contract InteractionsTest is Test {
         );
         console.log("Funder balance before funding: %s", preFundFunderBalance);
         console.log("Owner balance before funding: %s", preFundOwnerBalance);
-
+        console.log(
+            "----------------------------------------------------------"
+        );
+        
         // Act
+
         // Fund
-        vm.deal(FUNDER, fundAmount); // Make sure FUNDER has enough ETH.
-        vm.prank(FUNDER);
-        fundMe.fund{value: fundAmount}();
+        console.log("Funding from the FUNDER...");
+        vm.prank(FUNDER); // FUNDER will be the msg.sender for the next transaction only.
+        fundMe.fund{value: fundAmount}(); // Directly call the fund function send value
 
         uint256 afterFundBalance = address(fundMe).balance;
         uint256 afterFundFunderBalance = address(FUNDER).balance;
@@ -57,10 +65,13 @@ contract InteractionsTest is Test {
             afterFundBalance
         );
         console.log("Funder balance after funding: %s", afterFundFunderBalance);
+        console.log(
+            "----------------------------------------------------------"
+        );
 
         // Withdraw
-        vm.prank(fundMe.i_owner());
-        fundMe.withdraw();
+        WithdrawContract withdrawFundMe = new WithdrawContract();
+        withdrawFundMe.withdrawFundMe(address(fundMe));
         uint256 afterWithdrawBalance = address(fundMe).balance;
         uint256 afterWithdrawOwnerBalance = fundMe.i_owner().balance;
         console.log(
@@ -71,31 +82,67 @@ contract InteractionsTest is Test {
             "Owner balance after withdrawinging: %s",
             afterWithdrawOwnerBalance
         );
+
         // Assert
-        assertEq(
-            address(fundMe).balance,
-            0,
-            "FundMe should have 0 balance after withdrawal"
-        );
-        assertEq(
-            afterFundBalance,
-            preFundBalance + fundAmount,
-            "FundMe should receive fundAmount eth"
-        );
-        assertEq(
-            preFundOwnerBalance + fundAmount,
-            afterWithdrawOwnerBalance,
-            "Withdrawer should receive funds"
-        );
-    }
-
-    function testUserCanFundInteractions() public {
-        FundContract fundFundMe = new FundContract();
-        fundFundMe.fundFundMe(address(fundMe));
-
-        WithdrawContract withdrawFundMe = new WithdrawContract();
-        withdrawFundMe.withdrawFundMe(address(fundMe));
 
         assert(address(fundMe).balance == 0);
+        assertEq(afterWithdrawOwnerBalance, preFundFunderBalance + preFundOwnerBalance);
     }
+    // function testUserCanWithdrawFunds() public {
+    //     // Arrange
+    //     vm.deal(FUNDER, fundAmount);
+    //     uint256 preFundBalance = address(fundMe).balance;
+    //     uint256 preFundFunderBalance = address(FUNDER).balance;
+    //     uint256 preFundOwnerBalance = fundMe.i_owner().balance;
+    //     console.log(
+    //         "FundMe contract balance before funding: %s",
+    //         preFundBalance
+    //     );
+    //     console.log("Funder balance before funding: %s", preFundFunderBalance);
+    //     console.log("Owner balance before funding: %s", preFundOwnerBalance);
+
+    //     // Act
+    //     // Fund
+    //     vm.deal(FUNDER, fundAmount); // Make sure FUNDER has enough ETH.
+    //     vm.prank(FUNDER);
+    //     fundMe.fund{value: fundAmount}();
+
+    //     uint256 afterFundBalance = address(fundMe).balance;
+    //     uint256 afterFundFunderBalance = address(FUNDER).balance;
+    //     console.log(
+    //         "FundMe contract balance after funding: %s",
+    //         afterFundBalance
+    //     );
+    //     console.log("Funder balance after funding: %s", afterFundFunderBalance);
+
+    //     // Withdraw
+    //     vm.prank(fundMe.i_owner());
+    //     fundMe.withdraw();
+    //     uint256 afterWithdrawBalance = address(fundMe).balance;
+    //     uint256 afterWithdrawOwnerBalance = fundMe.i_owner().balance;
+    //     console.log(
+    //         "FundMe contract balance after withdrawing: %s",
+    //         afterWithdrawBalance
+    //     );
+    //     console.log(
+    //         "Owner balance after withdrawinging: %s",
+    //         afterWithdrawOwnerBalance
+    //     );
+    //     // Assert
+    //     assertEq(
+    //         address(fundMe).balance,
+    //         0,
+    //         "FundMe should have 0 balance after withdrawal"
+    //     );
+    //     assertEq(
+    //         afterFundBalance,
+    //         preFundBalance + fundAmount,
+    //         "FundMe should receive fundAmount eth"
+    //     );
+    //     assertEq(
+    //         preFundOwnerBalance + fundAmount,
+    //         afterWithdrawOwnerBalance,
+    //         "Withdrawer should receive funds"
+    //     );
+    // }
 }
